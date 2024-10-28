655bfcb59f0071c5c2ab45ae498c24e8b82265ea
Zeeshan Sheikh <zeeshan@makeen.io>
2024-10-28 18:21:22 +0500

    Update ExploreVC.swift

--- LocationServices/LocationServices/Scenes/Explore/Controller/ExploreVC.swift
+++ LocationServices/LocationServices/Scenes/Explore/Controller/ExploreVC.swift
@@ -47,7 +47,6 @@ final class ExploreVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //self.hideKeyboardWhenTappedAround()
         setupHandlers()
         setupNotifications()
         
